2aa20d79c548409bb67ee787888a61dd0d040a44
Noobysystem <99871227+Noobysystem@users.noreply.github.com>
2023-08-06 21:23:48 +0500

    Add files via upload

---
+++ alien_invasion.py
@@ -0,0 +1,44 @@
+import sys
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры."""
+    
+    def __init__(self):
+        """Иницилизирует игру и создает игровые ресурсы."""
+        pygame.init()
+        self.settings = Settings()
+        
+        # Назначение цвета фона.
+        self.bg_color = (230, 230, 230)
+        
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_heigh))
+        
+        pygame.display.set_caption("Alien Invasion")
+        
+        self.ship = Ship()
+        
+    def run_game(self):
+        """Запуск основного цикла игры."""
+        while True:
+            # Отслеживание событий клавиатуры и мыши.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            # При каждом проходе цикла перерисовывается экран.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+            
+            # Отоброжение последнего прорисованного экрана.
+            pygame.display.flip()
+            
+if __name__ == '__main__':
+    # Создание экземпляра и запуск игры.
+    ai = AlienInvasion()
+    ai.run_game()
