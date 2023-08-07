cc8a171457ddbbed50f572f5153ac88dc6edab86
noobysystem <noobysystem96@gmail.com>
2023-08-07 14:20:45 +0500

    add

--- settings.py
+++ settings.py
@@ -3,7 +3,8 @@ class Settings():
     
     def __init__(self):
         """Инициализирует настройки игры."""
-        # Папраметры экрана.
+        # Параметры экрана.
         self.screen_width = 1200
-        self.screen_heigh = 800
+        self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -1,19 +1,37 @@
 import pygame
 
+
 class Ship():
     """Класс для управления кораблем."""
-    
+
     def __init__(self, ai_game):
         """Инициализирует корабль и задает его начальную позицию."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
-        
+
         # Загружает изображение корабля и получает прямоугольник.
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/ship.png')
         self.rect = self.image.get_rect()
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
-        
+
+        # Сохранение вещественной координаты центра корабля.
+        self.x = float(self.rect.x)
+        # Флаг перемещения.
+        self.moving_right = False
+        self.moving_left = False
+    def update(self):
+        """Обновляет позицию корабля с учетом флага."""
+        # Обновляется атрибут x, а не rect.
+        if self.moving_right:
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+
+        # Обновление атрибута rect на основании self.x.
+        self.rect.x = self.x
+
     def blitme(self):
         """Рисует корабль в текущей позиции."""
         self.screen.blit(self.image, self.rect)
